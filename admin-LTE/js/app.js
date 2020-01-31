bb779a33c8e30afd9a341efaf54a8a20f8becf11
Samuel Surya <samuel.surya@libertyjaya.com>
2020-01-31 09:18:58 +0700

    add skins j

--- admin-LTE/js/app.js
+++ admin-LTE/js/app.js
@@ -36,8 +36,8 @@ $.AdminLTE.options = {
   //Add slimscroll to navbar menus
   //This requires you to load the slimscroll plugin
   //in every page before app.js
-  navbarMenuSlimscroll: true,
-  navbarMenuSlimscrollWidth: "3px", //The width of the scroll bar
+  navbarMenuSlimscroll: false,
+  navbarMenuSlimscrollWidth: "12px", //The width of the scroll bar
   navbarMenuHeight: "200px", //The height of the inner menu
   //General animation speed for JS animated elements such as box collapse/expand and
   //sidebar treeview slide up/down. This options accepts an integer as milliseconds,
